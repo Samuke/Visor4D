1f5780c66bbb4dfffb09e4ed3f2f13775c47d1d7
Pamela Lincoqueo <plincoqueo2015@alu.uct.cl>
2018-10-29 09:15:01 -0300

    Agregado linea de tiempo, aún no funcional

---
+++ linea.js
@@ -0,0 +1,19 @@
+var parentH = $(".linT").height();
+
+// var jsonCircles = [{"x_axis": 70, "y_axis": parentH/2, "radius": 40, "color" : "blue"}];
+
+// var svgContainer = d3.select(".linT").append("svg")
+		// .attr("width", 200)
+		// .attr("height", 200);
+
+// var circles = svgContainer.selectAll("circle")
+		// .data(jsonCircles)
+		// .enter()
+		// .append("circle");
+
+// var circleAttributes = circles
+		// .attr("cx", function (d) { return d.x_axis; })
+		// .attr("cy", function (d) { return d.y_axis; })
+		// .attr("r", function (d) { return d.radius; })
+		// .style("fill", function(d) { return d.color; });
+$(".play").click(function(){ alert("Click");});
